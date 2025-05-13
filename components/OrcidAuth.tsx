b3fdb8b65cc3c4ff40245a342809fe4442740d13
jonaschlegel <jona.schlegel@gmail.com>
2025-05-12 23:10:15 -0700

    Reverse orcid visible information in UI

--- components/OrcidAuth.tsx
+++ components/OrcidAuth.tsx
@@ -27,27 +27,33 @@ export default function OrcidAuth() {
   }
 
   return (
-    <div className="flex items-center space-x-1 sm:space-x-3">
+    <div className="flex items-center space-x-3">
       {!session ? (
         <Button
           onClick={() =>
             signIn('orcid', { callbackUrl: window.location.origin })
           }
           variant="secondary"
-          aria-label="Sign in with ORCID"
-          className="p-2 h-8 w-8 flex items-center justify-center"
         >
-          <LogIn className="h-5 w-5" />
+          <LogIn className="h-4 w-4 mr-1" />
+          Sign in
         </Button>
       ) : (
-        <Button
-          onClick={() => signOut()}
-          variant="default"
-          aria-label="Sign out"
-          className="p-2 h-8 w-8 flex items-center justify-center"
-        >
-          <LogOut className="h-5 w-5" />
-        </Button>
+        <>
+          <span className="text-sm text-white">
+            {(session.user as SessionUser)?.label}
+            <br />
+            <small>ORCID: {(session.user as SessionUser)?.id}</small>
+          </span>
+          <Button
+            onClick={() => signOut()}
+            variant="default"
+            className="hover:text-secondary"
+          >
+            <LogOut className="h-4 w-4 mr-1" />
+            Sign out
+          </Button>
+        </>
       )}
     </div>
   );
