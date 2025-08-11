b9a278b8893f8f91eadbc691a421a06eca46352c
Jona Schlegel <38097631+jonaschlegel@users.noreply.github.com>
2025-08-11 20:33:40 +0200

    Update middleware.ts to fix netlify issue

--- middleware.ts
+++ middleware.ts
@@ -33,5 +33,6 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 export const config = {
+  runtime: 'nodejs',
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|image/).*)'],
 };
