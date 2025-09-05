e1299a8b0b11834356a1b1da213e67051c0772dd
jonaschlegel <jona.schlegel@gmail.com>
2025-09-05 19:50:25 +0200

    Fix: log in and hydration error

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,12 +1,22 @@
 'use client';
 
-import * as React from 'react';
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps as NextThemesProviderProps,
 } from 'next-themes';
+import * as React from 'react';
 
 export interface ThemeProviderProps extends NextThemesProviderProps {}
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
