d06d958aef73a24850893cce741f26938ad100e5
Jona Schlegel <jona.schlegel@gmail.com>
2026-02-24 17:17:55 +0100

    Update the iconography linking

--- components/IconographyThesaurus.tsx
+++ components/IconographyThesaurus.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { ExternalLink } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 interface ThesaurusConcept {
   '@id': string;
